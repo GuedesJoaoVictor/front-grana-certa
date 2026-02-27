c50eb3d0209738ab26b5e5e2c916347640455dbe
Gue10 <joaoguedes1404@gmail.com>
2026-02-27 11:23:20 -0300

    feat - Adicionando rota para o backend

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,7 +1,24 @@
 import { useAuth } from "@/auth/AuthProvider";
-import { useState } from "react";
+import { api } from "@/config/axios";
+import { UserDTO } from "@/core/user.dto";
+import { useEffect, useState } from "react";
 
 export function useUser() {
     const { keycloak } = useAuth();
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState<UserDTO | null>(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await api.get("/test/private/me");
+                console.log("User data:", response);
+                setUser(response.data);
+            } catch (error) {
+                console.error("Failed to fetch user data:", error);
+            }
+        };
+        fetchUser();
+    }, [keycloak]);
+
+    return user;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,19 @@
 import { SideBar } from "@/components/SideBar/SideBar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useUser } from "@/hooks/useAuth";
 
 export function Home() {
+
+    const user = useUser();
+
+    if (!user) {
+        return (
+            <div>
+                <h1>Loading user data...</h1>
+            </div>
+        );
+    }
+
     return (
         <SidebarProvider>
             <SideBar />
